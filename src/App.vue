bcedbceb6f79a1eb8a7aa5897606874f1a2143bd
Carlos La Fuente Sanguino <crlafuente@fassil.com.bo>
2021-06-19 19:32:38 -0400

    Base project with tailwind created

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
